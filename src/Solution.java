84b2015c77af971ffe5b061baaf79a57d1269aa2
pjmoo <_00pjmoooo@gmail.com>
2026-06-23 17:23:52 +0900

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+    public boolean solution(String s) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        String[] arr = s.split("");
+        for (String v : arr) {
+
+            if (v.equals("(")) {
+
+                stack.push(1);
+            }
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+
+                int top = stack.peek();
+                stack.pop();
+            }
+        }
+
+
+        boolean answer = (stack.size() == 0);
+        return answer;
+    }
+}
